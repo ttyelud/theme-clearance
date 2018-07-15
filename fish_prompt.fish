f43ce0e5a5dcbc168598cf02d04be8bf41031d85
ttyelud <joshua.peele@gmail.com>
2018-07-15 00:17:29 -0400

    Misc

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,61 +1,54 @@
-# name: clearance
-# ---------------
-# Based on idan. Display the following bits on the left:
-# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
-# - Current directory name
-# - Git branch and dirty state (if inside a git repo)
+# fish theme: gonefather
 
 function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
-function _git_is_dirty
+function _is_git_dirty
   echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
-function fish_prompt
-  set -l last_status $status
+# change color depending on the user.
+function _user_host
+  if [ (id -u) = "0" ];
+    echo -n (set_color -o red)
+  else
+    echo -n (set_color -o blue)
+  end
+  echo -n (hostname|cut -d . -f 1)ˇ$USER (set color normal)
+end
 
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
+function fish_prompt
+  set fish_greeting
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
   set -l normal (set_color normal)
 
-  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
+  set -l cwd $cyan(basename (prompt_pwd))
 
-  # Output the prompt, left to right
+  # output the prompt, left to right:
+  # display 'user@host:'
+  echo -n -s $green (whoami) $dark_green @ $green (hostname|cut -d . -f 1) ": "
 
-  # Add a newline before new prompts
-  echo -e ''
-
-  # Display [venvname] if in a virtualenv
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
-  end
-
-  # Print pwd or full path
+  # display the current directory name:
   echo -n -s $cwd $normal
 
-  # Show git branch and status
+  # show git branch and dirty state, if applicable:
   if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
+    set -l git_branch '[' (_git_branch_name) ']'
 
-    if [ (_git_is_dirty) ]
-      set git_info '(' $yellow $git_branch "±" $normal ')'
+    if [ (_is_git_dirty) ]
+      set git_info $red $git_branch "×"
     else
-      set git_info '(' $green $git_branch $normal ')'
+      set git_info $green $git_branch
     end
-    echo -n -s ' · ' $git_info $normal
+    echo -n -s ' ' $git_info $normal
   end
 
-  set -l prompt_color $red
-  if test $last_status = 0
-    set prompt_color $normal
-  end
+  # terminate with a nice prompt char:
+  echo -n -s ' > ' $normal
 
-  # Terminate with a nice prompt char
-  echo -e ''
-  echo -e -n -s $prompt_color '⟩ ' $normal
 end
